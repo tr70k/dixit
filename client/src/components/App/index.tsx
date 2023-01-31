b489b558a88f6f3cbfd284bcbb2b1c8328c14735
tr70k <tr70k.code@gmail.com>
2023-01-31 21:38:36 +0100

    add styled-components

--- client/src/components/App/index.tsx
+++ client/src/components/App/index.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.png';
-import './App.css';
+import { AppHeader, AppLink, AppLogo, Wrapper } from './styled';
 
-function App() {
+function Index() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </Wrapper>
   );
 }
 
-export default App;
+export default Index;
